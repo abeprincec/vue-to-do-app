39d561f2464b4f5fbe1cc54c5425f4913e48cab1
Abraham Prince <abeprincec@gmail.com>
2018-09-01 22:25:16 -0400

    fix errors

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
- <input v-model="message" placeholder="edit me">
+ <input v-model="message" placeholder="Add to list">
  
     <h2>To Do List</h2>  
     <p>{{ message }}</p>  
